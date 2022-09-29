2f9526940f1925ab217821801cad7f8e1c43c1ed
kalekye <bettysbrenda@gmail.com>
2022-09-29 10:16:55 +0300

    Finished exercise 7.7

--- ex_7_07.py
+++ ex_7_07.py
@@ -2,15 +2,7 @@ file=input('Enter file name: ');
 fhand=open(file);
 
 for line in fhand:
-    line=line.replace('\n\n',' ')
     line=line.rstrip();
-    nline=line.splitlines();
-    if nline=='':
-        break;
-    print(nline);
-    #lines=' '.join(nline);
-    #print(lines);
-    #words=lines.split();
-    #if words[0]=='From:':
-        #print(words);
-    
+    if line.startswith('From '):
+        words=line.split();
+        print(words[2]);
